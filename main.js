c14f84b642e98b76e7abe57a79891da83ef41955
Daniel Hernández <dant.hdez02@gmail.com>
2023-03-23 23:14:45 -0600

    Calculator Functionalities, eval(), parseInt()

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-const btnNumberList = document.querySelectorAll('input');
+const form = document.forms[0];
+const display = document.getElementById('display');
+const equals = document.getElementById('equals');
 
 
-function setValueToNode(){
-    for(let i = 2; i < btnNumberList.length; i++){
-       btnNumberList[i].addEventListener('click', ()=> {
-            display.value += btnNumberList[i].value;
-       });
+display.addEventListener('input', () => {
+    parseInt(display.value);
+    if(isNaN(display.value) == true) {
+        alert("Oops! You must use numbers");
+        display.value = "";
     }
+})
+
+form.addEventListener('click', (e) => {
+     display.value += e.target.value;
+})
+
+equals.onclick = function parse() {
+    parseInt(display.value);
+    let result = eval(display.value);
+
+    return display.value = result;
 }
 
-setValueToNode();
 
-console.log(btnNumberList)
